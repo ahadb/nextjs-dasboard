e65c210f55c5a2616bcc4a7627780c1621cac396
ahadb <ahadbokhari@gmail.com>
2024-01-13 18:06:03 -0500

    chore: initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+// secondary font
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
